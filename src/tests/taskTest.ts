2ae3d516ad4f31e8308a887b7c8d5b7e020d403c
chodkowski <piotr.chodkowski4@gmail.com>
2025-01-15 18:43:31 +0100

    fix: fix typing

--- src/tests/taskTest.ts
+++ src/tests/taskTest.ts
@@ -5,12 +5,13 @@ import { mapCategories } from '../helpers/mapCategories';
 import { setHomeCategories } from '../helpers/setHomeCategories';
 import { getCategories } from '../mockedApi';
 import { categoryTree } from '../task';
+import { Category, CategoryListElement } from '../types';
 
 //1. Not too familiar with AVA testing lib, but looks like deepEqual is covering our needs.
 //  if not we can always JSON.stringify both values to compare and check it with t.is()
 //2. Wasn't sure if I should add more test, so I focused on task itself
 test('check if categoryTree is returning correct value', async (t) => {
-  const result = await categoryTree(
+  const result = await categoryTree<Category, CategoryListElement>(
     getCategories,
     mapCategories,
     setHomeCategories
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
+export interface Category {
+  id: number;
+  name: string;
+  hasChildren: boolean;
+  url: string;
+  Title: string;
+  MetaTagDescription: string;
+  children: Category[];
+}
+
 export interface CategoryListElement {
   children: CategoryListElement[];
   image: string;
